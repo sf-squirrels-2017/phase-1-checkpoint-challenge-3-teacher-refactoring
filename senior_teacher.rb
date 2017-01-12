24e3142c6158c28fa7d6f87b6b8d3a38484c32b1
slinkous <sralinkous@gmail.com>
2017-01-12 09:51:22 -0800

    Refactor Apprentice and Senior to inherit from Teacher

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,39 +1,29 @@
-class SeniorTeacher
+require_relative "teacher"
+
+class SeniorTeacher < Teacher
+  
   attr_reader :age, :salary, :phase, :performance_rating, :target_raise
   attr_accessor :name
 
   def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
     @target_raise = 1000
+    @performance_rating = options.fetch(:performance_rating)
   end
 
-  def offer_high_five
-    "High five!"
-  end
 
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
 
   def teach_stuff
     response = ""
     response += "Listen, class, this is how everything works, fo SHO! "
     response += "*drops flat-out insane knowledge bomb* "
-    response += "... You're welcome. *saunters away*"
+    response += "... You're welcome."
+    response += " *saunters away*"
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
 
-  def receive_raise(raise)
-    @salary += raise
-  end
+
 
   def set_performance_rating(rating)
     response = ""
