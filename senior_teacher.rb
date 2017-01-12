dc7b04dabc5f963f1e5736d005dbbfeeb39d3ce3
George Babayan <ggbabayan@gmail.com>
2017-01-12 09:57:21 -0800

    Add module for teachers and teacher class

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,21 +1,17 @@
-class SeniorTeacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  attr_accessor :name
+require_relative 'action_for_teachers'
+require_relative 'teacher'
+
+class SeniorTeacher < Teacher
+  include Shouts
+  include Salary
+  include Teacher_Phase
+
+  attr_reader :performance_rating
 
   def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
     @target_raise = 1000
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
+    @max_rating = 90
+    super
   end
 
   def teach_stuff
@@ -23,27 +19,6 @@ class SeniorTeacher
     response += "Listen, class, this is how everything works, fo SHO! "
     response += "*drops flat-out insane knowledge bomb* "
     response += "... You're welcome. *saunters away*"
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
     response
   end
 
