6d72d9f98b5b1d300e32d114b1fe3b3c374eb17e
Vilde Vevatne <vilde@vevatne.no>
2017-01-12 09:41:54 -0800

    makes high-five module

--- educator.rb
+++ educator.rb
@@ -27,4 +27,16 @@ class Educator
     @salary += raise
   end
 
+  def set_performance_rating(rating)
+    response = ""
+    if rating > 90
+      response = "Yay, I'm a great employee!"
+      receive_raise(@target_raise)
+    else
+      response += "Oh, well -- thanks to this actionable, specific, and kind "
+      response += "feedback, I'll do better next time."
+    end
+    response
+  end
+
 end
