25628b4b9adea1eeb4d036a05022c697e12b762e
Vilde Vevatne <vilde@vevatne.no>
2017-01-12 09:50:27 -0800

    cleans up attr_reader in educator

--- educator.rb
+++ educator.rb
@@ -1,6 +1,6 @@
 class Educator
 
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
+  attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
   def initialize(options={})
@@ -13,13 +13,13 @@ class Educator
     "Cool, I've always wanted to teach phase #{num}!"
   end
 
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works "
-    response += "*drops flat-out insane knowledge bomb* "
-    response += "... You're welcome."
-    response
-  end
+  # def teach_stuff
+  #   response = ""
+  #   response += "Listen, class, this is how everything works "
+  #   response += "*drops flat-out insane knowledge bomb* "
+  #   response += "... You're welcome."
+  #   response
+  # end
 
   def salary=(new_salary)
     puts "This better be good!"
@@ -30,16 +30,16 @@ class Educator
     @salary += raise
   end
 
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
+  # def set_performance_rating(rating)
+  #   response = ""
+  #   if rating > 90
+  #     response = "Yay, I'm a great employee!"
+  #     receive_raise(@target_raise)
+  #   else
+  #     response += "Oh, well -- thanks to this actionable, specific, and kind "
+  #     response += "feedback, I'll do better next time."
+  #   end
+  #   response
+  # end
 
 end
