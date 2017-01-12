e27b9376998a13a88b5ec25e09ab204558473eb8
Vilde Vevatne <vilde@vevatne.no>
2017-01-12 09:39:23 -0800

    makes educator class with common methods from the educators

---
+++ educator.rb
@@ -0,0 +1,30 @@
+class Educator
+
+  def initialize(options={})
+    @age = options.fetch(:age, 0)
+    @name = options.fetch(:name, "")
+  end
+
+  def set_phase(num)
+    @phase = num
+    "Cool, I've always wanted to teach phase #{num}!"
+  end
+
+  def teach_stuff
+    response = ""
+    response += "Listen, class, this is how everything works "
+    response += "*drops flat-out insane knowledge bomb* "
+    response += "... You're welcome."
+    response
+  end
+
+  def salary=(new_salary)
+    puts "This better be good!"
+    @salary = new_salary
+  end
+
+  def receive_raise(raise)
+    @salary += raise
+  end
+
+end
