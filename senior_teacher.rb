15e380c29b515195eb43213ffa3835f3f9916f8d
Aaron Calander <email": null>
2017-01-12 10:14:31 -0800

    Refactored all classes and created mixin module named Teach

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,6 +1,9 @@
+require './teach'
+
 class SeniorTeacher
   attr_reader :age, :salary, :phase, :performance_rating, :target_raise
   attr_accessor :name
+  include Teach
 
   def initialize(options={})
     @phase = 3
@@ -9,30 +12,12 @@ class SeniorTeacher
     @target_raise = 1000
   end
 
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
-
   def teach_stuff
     response = ""
     response += "Listen, class, this is how everything works, fo SHO! "
     response += "*drops flat-out insane knowledge bomb* "
     response += "... You're welcome. *saunters away*"
     response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
